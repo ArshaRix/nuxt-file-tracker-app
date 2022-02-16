93ea06a54855da8a986645fc7c3a7b57daacd697
ArshaRix <jericoffee15@gmail.com>
2022-02-16 21:33:00 +0800

    Added dotenv

--- api/config/mongodb.js
+++ api/config/mongodb.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose')
 const db = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI, {
+            useUnifiedTopology: true,
             useNewUrlParser: true,
-            useUnifiedTopology: true,        
+            useCreateIndex: true      
         })
 
     } catch (err) {
